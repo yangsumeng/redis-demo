6c863038235dd1f1b88c53e1669fd327e6691377
yangsumeng <584903541@qq.com>
2019-10-20 23:57:04 +0800

    temp

--- src/test/java/com/RedisApplicationTest.java
+++ src/test/java/com/RedisApplicationTest.java
@@ -7,10 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RedisApplication.class)
 public class RedisApplicationTest {
-    @RunWith(SpringRunner.class)
-    @SpringBootTest(classes = RedisApplication.class)
-    public class MainTest {
+
 //        @Autowired
 //        private AService aService;
 
@@ -24,5 +25,4 @@ public class RedisApplicationTest {
 //            aService.doSmthing();
             System.out.println("--------------测试----------");
         }
-    }
 }
---
+++ src/test/java/test/com/RedisApplicationTestTest.java
@@ -0,0 +1,45 @@
+package test.com; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* RedisApplicationTest Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ê®ÔÂ 20, 2019</pre> 
+* @version 1.0 
+*/ 
+public class RedisApplicationTestTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: before() 
+* 
+*/ 
+@Test
+public void testBefore() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: testStartUp() 
+* 
+*/ 
+@Test
+public void testTestStartUp() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
